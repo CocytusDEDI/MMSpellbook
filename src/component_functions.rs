5a94fb1e65e8bf33a0708746514a7783c042cc0d
CocytusDEDI <cocytusdedi@tutanota.com>
2024-07-11 17:30:43 +0100

    Adding ability to run bytecode

--- src/component_functions.rs
+++ src/component_functions.rs
@@ -1,16 +1,29 @@
+use godot::{builtin::Vector3, log::godot_print, obj::WithBaseField};
+
 use crate::Spell;
 
+const APPLY_TO_SPELL_COEFFICIENT: f64 = 100.0;
 
-pub fn give_velocity(spell: &mut Spell, parameters: &[u32], should_execute: bool) -> Option<u32> {
-    let parameter_one: f32 = f32::from_bits(parameters[0]);
-    let parameter_two: f32 = f32::from_bits(parameters[1]);
-    let parameter_three: f32 = f32::from_bits(parameters[2]);
+pub fn give_velocity(spell: &mut Spell, parameters: &[u64], should_execute: bool) -> Option<u64> {
+    let delta: f32 = match parameters[0] {
+        0 => 1.0 as f32,
+        num => f64::from_bits(num) as f32
+    };
+    godot_print!("{:?}", spell.energy);
+    let parameter_one: f32 = f64::from_bits(parameters[1]) as f32;
+    let parameter_two: f32 = f64::from_bits(parameters[2]) as f32;
+    let parameter_three: f32 = f64::from_bits(parameters[3]) as f32;
     match should_execute {
-        false => Some(f32::to_bits(((spell.energy / 10.0) * ((parameter_one * parameter_one + parameter_two * parameter_two + parameter_three * parameter_three) as f64).sqrt()) as f32)),
-        true => None
+        false => Some(f64::to_bits((spell.energy / APPLY_TO_SPELL_COEFFICIENT) * ((parameter_one * parameter_one + parameter_two * parameter_two + parameter_three * parameter_three) as f64).sqrt())),
+        true => {
+            let previous_position = spell.base_mut().get_position();
+            let new_position = previous_position + Vector3 {x: parameter_one * delta, y: parameter_two * delta, z: parameter_three * delta};
+            spell.base_mut().set_position(new_position);
+            return None
+        }
     }
 }
 
-pub fn example_function_two(spell: &mut Spell, parameters: &[u32], should_execute: bool) -> Option<u32> {
+pub fn example_function_two(spell: &mut Spell, parameters: &[u64], should_execute: bool) -> Option<u64> {
     return None
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ use lazy_static::lazy_static;
 use serde_json::Value;
 use std::collections::HashMap;
 
+// When a spell has energy below this level it is discarded as being insignificant
+const ENERGY_CONSIDERATION_LEVEL: f64 = 0.001;
+
 struct MyExtension;
 
 #[gdextension]
@@ -22,9 +25,9 @@ mod component_functions;
 struct Spell {
     base: Base<Area3D>,
     energy: f64,
-    ready_instructions: Vec<u32>,
-    process_instructions: Vec<u32>,
-    component_efficiencies: Option<HashMap<u32, f64>>
+    ready_instructions: Vec<u64>,
+    process_instructions: Vec<u64>,
+    component_efficiencies: Option<HashMap<u64, f64>>
 }
 
 
@@ -33,10 +36,17 @@ impl IArea3D for Spell {
     fn init(base: Base<Area3D>) -> Self {
         Self {
             base,
-            energy: 0.0,
+            energy: 10.0,
             ready_instructions: vec![],
             process_instructions: vec![],
-            component_efficiencies: None
+            component_efficiencies: None // Replace None with the following commented out code to run manually without providing efficiencies through json:
+            /*
+                {
+                let mut component_efficiencies_map = HashMap::new();
+                component_efficiencies_map.insert(0, 0.5);
+                Some(component_efficiencies_map)
+            }
+            */
         }
     }
 
@@ -46,42 +56,50 @@ impl IArea3D for Spell {
         collision_shape.set_shape(shape.upcast::<Shape3D>());
         self.base_mut().add_child(collision_shape.upcast());
         self.base_mut().add_child(CsgSphere3D::new_alloc().upcast());
-
-        self.spell_virtual_machine(&self.ready_instructions.clone());
+        self.spell_virtual_machine(&self.ready_instructions.clone(), None);
+        if self.energy < ENERGY_CONSIDERATION_LEVEL {
+            self.base_mut().queue_free();
+        }
     }
 
     fn physics_process(&mut self, delta: f64) {
-        self.spell_virtual_machine(&self.process_instructions.clone());
+        self.spell_virtual_machine(&self.process_instructions.clone(), Some(delta));
+        if self.energy < ENERGY_CONSIDERATION_LEVEL {
+            self.base_mut().queue_free();
+        }
     }
 }
 
 lazy_static! {
-    static ref COMPONENT_TO_FUNCTION_MAP: HashMap<u32, (fn(&mut Spell, &[u32], bool) -> Option<u32>, u32)> = {
+    static ref COMPONENT_TO_FUNCTION_MAP: HashMap<u64, (fn(&mut Spell, &[u64], bool) -> Option<u64>, u64)> = {
         let mut component_map = HashMap::new();
-        component_map.insert(0, (component_functions::give_velocity as fn(&mut Spell, &[u32], bool) -> Option<u32>, 3));
+        component_map.insert(0, (component_functions::give_velocity as fn(&mut Spell, &[u64], bool) -> Option<u64>, 3));
         return component_map
     };
 }
 
 
 impl Spell {
-    fn spell_virtual_machine(&mut self, instructions: &[u32]) -> Result<(), ()> {
+    fn spell_virtual_machine(&mut self, instructions: &[u64], option_delta: Option<f64>) -> Result<(), ()> {
         // ToDo: Code such as rpn evaluation should be in it's own subroutine to be available to call for other logic statements.
         // ToDo: Decide to remove or not remove strings from Parameter
-
+        let delta: u64 = match option_delta {
+            Some(time) => f64::to_bits(time),
+            None => f64::to_bits(0.0)
+        };
         let mut instructions_iter = instructions.iter();
         while let Some(bits) = instructions_iter.next() {
             match bits {
                 103 => { // 103 = component
                     let component_code = *instructions_iter.next().expect("Expected component");
                     let number_of_component_parameters = self.get_number_of_component_parameters(component_code);
-                    let mut parameters: Vec<u32> = vec![];
+                    let mut parameters: Vec<u64> = vec![delta];
                     for _ in 0..number_of_component_parameters {
                         parameters.push(*instructions_iter.next().expect("Expected parameter"));
                     }
                     self.call_component(component_code, parameters)?;
                 },
-                400 => {
+                400 => { // 400 = if statement
 
                 },
                 _ => panic!("Not valid opcode")
@@ -91,12 +109,12 @@ impl Spell {
     }
 
 
-    fn call_component(&mut self, component_code: u32, parameters: Vec<u32>) -> Result<Option<u32>, ()> {
+    fn call_component(&mut self, component_code: u64, parameters: Vec<u64>) -> Result<Option<u64>, ()> {
         if let Some((function, _)) = COMPONENT_TO_FUNCTION_MAP.get(&component_code) {
             if let Some(component_efficiencies) = self.component_efficiencies.clone() {
                 if let Some(efficiency) = component_efficiencies.get(&component_code) {
                     if let Some(base_energy) = function(self, &parameters, false) {
-                        let base_energy = f32::from_bits(base_energy) as f64;
+                        let base_energy = f64::from_bits(base_energy);
                         let energy_needed = base_energy / efficiency;
                         if self.energy >= energy_needed {
                             self.energy -= energy_needed;
@@ -122,7 +140,7 @@ impl Spell {
         }
     }
 
-    fn get_number_of_component_parameters(&self, component_code: u32) -> u32 {
+    fn get_number_of_component_parameters(&self, component_code: u64) -> u64 {
         if let Some((_, number_of_parameters)) = COMPONENT_TO_FUNCTION_MAP.get(&component_code) {
             return *number_of_parameters
         } else {
@@ -135,9 +153,9 @@ impl Spell {
 
         match serde_json::from_str(&json_string) {
             Ok(Value::Object(efficiencies_object)) => {
-                let mut temp_hashmap: HashMap<u32, f64> = HashMap::new();
+                let mut temp_hashmap: HashMap<u64, f64> = HashMap::new();
                 for (key, value) in efficiencies_object {
-                    if let (Ok(parsed_key), Some(parsed_value)) = (key.parse::<u32>(), value.as_f64()) {
+                    if let (Ok(parsed_key), Some(parsed_value)) = (key.parse::<u64>(), value.as_f64()) {
                         temp_hashmap.insert(parsed_key, parsed_value);
                     }
                 }
@@ -149,7 +167,7 @@ impl Spell {
     }
 }
 
-fn custom_bool_and(first: u32, second: u32) -> u32 {
+fn custom_bool_and(first: u64, second: u64) -> u64 {
     // 100 = true, 101 = false
     match (first, second) {
         (100, 100) => 100,
@@ -160,7 +178,7 @@ fn custom_bool_and(first: u32, second: u32) -> u32 {
     }
 }
 
-fn custom_bool_or(first: u32, second: u32) -> u32 {
+fn custom_bool_or(first: u64, second: u64) -> u64 {
     // 100 = true, 101 = false
     match (first, second) {
         (100, 100) => 100,
@@ -171,7 +189,7 @@ fn custom_bool_or(first: u32, second: u32) -> u32 {
     }
 }
 
-fn custom_bool_xor(first: u32, second: u32) -> u32 {
+fn custom_bool_xor(first: u64, second: u64) -> u64 {
     // 100 = true, 101 = false
     match (first, second) {
         (100, 100) => 101,
@@ -182,7 +200,7 @@ fn custom_bool_xor(first: u32, second: u32) -> u32 {
     }
 }
 
-fn custom_bool_not(first: u32) -> u32 {
+fn custom_bool_not(first: u64) -> u64 {
     // 100 = true, 101 = false
     match first {
         100 => 101,
