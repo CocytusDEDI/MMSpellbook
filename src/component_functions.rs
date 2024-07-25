928009ee6b83700843d41d64fa44d922afd2e958
AndycptSher <andycpt20@gmail.com>
2024-07-25 10:11:02 +0100

    Minor refactor

--- src/component_functions.rs
+++ src/component_functions.rs
@@ -8,12 +8,11 @@ pub fn give_velocity(spell: &mut Spell, parameters: &[u64], should_execute: bool
     let parameter_one: f32 = f64::from_bits(parameters[0]) as f32;
     let parameter_two: f32 = f64::from_bits(parameters[1]) as f32;
     let parameter_three: f32 = f64::from_bits(parameters[2]) as f32;
-    match should_execute {
-        false => Some(vec![f64::to_bits((spell.energy / 2.0) * ((parameter_one * parameter_one + parameter_two * parameter_two + parameter_three * parameter_three) as f64).sqrt() / APPLY_TO_SPELL_COEFFICIENT)]), // E_K = (1/2)mv^2
-        true => {
-            let new_velocity = spell.velocity + Vector3 {x: parameter_one, y: parameter_two, z: parameter_three };
-            spell.velocity = new_velocity;
-            return None
-        }
+    if !should_execute {
+        return Some(vec![f64::to_bits((spell.energy / 2.0) * ((parameter_one * parameter_one + parameter_two * parameter_two + parameter_three * parameter_three) as f64).sqrt() / APPLY_TO_SPELL_COEFFICIENT)]) // E_K = (1/2)mv^2
+    } else {
+        let new_velocity = spell.velocity + Vector3 {x: parameter_one, y: parameter_two, z: parameter_three };
+        spell.velocity = new_velocity;
+        return None
     }
 }
